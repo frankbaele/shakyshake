a868b6db43164601af8d50f9782016141e0dedd9
Michael Vandendriessche <michael.vandendriessche@ordina.be>
2016-01-31 00:42:54 +0100

    cleaned up beatAnimation script

--- Assets/scripts/animation/beatAnimation.cs
+++ Assets/scripts/animation/beatAnimation.cs
@@ -7,39 +7,24 @@ public class beatAnimation : MonoBehaviour {
 
     private int frameIndex = 0;
     private SpriteRenderer r;
+    private Animator anim;
     private bool continueAnimation = false;
     int count = 0;
-   // private GameObject shamanAnimation;
-    //private Animator anim;
 
     // Use this for initialization
     void Start () {
         r = GetComponent<SpriteRenderer>();
-       // anim = GetComponent<Animator>();
-        //anim.StartPlayback();
+        anim = GetComponent<Animator>();
         Events.instance.AddListener<TimerTick>(tick);
-        //shamanAnimation = GameObject.Find("ShamanAnimation");
     }
 
     // Update is called once per frame
     void Update() {
 
-        //r.sprite = frames[frameIndex];
-
     }
 
     void tick(TimerTick e)
     {   
-        if(count == 4)
-        {
-
-        }
-            //go to next step in animation
-            //frameIndex = frameIndex % frames.Length;
-            //Debug.Log("index" + frameIndex);
-            //r.sprite = frames[frameIndex];
-
-            //frameIndex++;
-        GetComponent<Animator>().Play("Shaman", 0, 5f / 7f);
+        anim.Play("Shaman", 0, 5f / 7f);
     }
 }
