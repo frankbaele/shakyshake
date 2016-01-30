9a8cc33e3b6b82b25ba109a73031d5665e403a75
Michael Vandendriessche <michael.vandendriessche@ordina.be>
2016-01-30 15:07:49 +0100

    added beatAnimation script

---
+++ Assets/scripts/animation/beatAnimation.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class beatAnimation : MonoBehaviour {
+    public Texture2D[] frames;
+
+    private int frameIndex;
+    private Renderer r;
+
+    // Use this for initialization
+    void Start () {
+        r = GetComponent<Renderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+
+    void tick(TimerTick e)
+    {
+        if (e.note % 2 == 0)
+        {
+            //go to next step in animation
+            r.material.mainTexture = frames[frameIndex];
+        }
+
+    }
+}
