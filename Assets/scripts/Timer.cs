02551fac8a194f807f9ff7c94e65501daf9e4657
Frank Baele <frankbaele@gmail.com>
2016-01-30 12:04:18 +0100

    add repeating

--- Assets/scripts/Timer.cs
+++ Assets/scripts/Timer.cs
@@ -3,26 +3,30 @@ using System.Collections;
 
 public class Timer : MonoBehaviour {
 	public int bpm;
-	bool playing = false;
+	float interval;
+	int note = 0;
 	// Use this for initialization
 	void Start () {
-		playing = true;
-		StartCoroutine("timer");
+		startTimer();
+
 	}
 	
+	void tick (){
+		Events.instance.Raise(new TimerTick(note, bpm, interval));
+		note = note == 15 ? 0 : note + 1;
+	}
+	
+	void startTimer(){
+		interval =  60/(float)bpm/4;
+		InvokeRepeating("tick", 0, interval);
+	}
+	void stopTimer(){
+		CancelInvoke("tick");
+	}
 	// Update is called once per frame
 	void Update () {
 	
 	}
-	
-	private IEnumerator timer(){
-		float interval = 60/(float)bpm/4;
-		int note = 0;
-		while(playing){
-			Events.instance.Raise(new TimerTick(note, bpm, interval));
-			note = note == 15 ? 0 : note + 1;
-			yield return new WaitForSeconds(interval);
-		}
-	}
+
 	
 }
--- Assets/scripts/sound/SoundManager.cs
+++ Assets/scripts/sound/SoundManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class SoundManager : MonoBehaviour {
 	public AudioClip kick;
+	public AudioClip snare;
 	private AudioSource audio;
 	// Use this for initialization
 	void Start () {
@@ -17,6 +18,9 @@ public class SoundManager : MonoBehaviour {
 	void tick(TimerTick e){
 		if(e.note%4 == 0){
 			audio.PlayOneShot(kick, 1F);
+		}
+		if(e.note%2 == 0){
+			//audio.PlayOneShot(snare, 1F);
 		}
 	}
 }
