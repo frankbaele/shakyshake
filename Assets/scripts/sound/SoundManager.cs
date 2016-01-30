4dbae103f902139b868d98fb7c4142f27cfc8e8a
Frank Baele <frankbaele@gmail.com>
2016-01-30 12:51:45 +0100

    more soundclips

--- Assets/scripts/sound/SoundManager.cs
+++ Assets/scripts/sound/SoundManager.cs
@@ -2,9 +2,19 @@
 using System.Collections;
 
 public class SoundManager : MonoBehaviour {
+private AudioSource audio;
+	public AudioClip clap;
+	public AudioClip closed;
+	public AudioClip crash;
+	public AudioClip high;
 	public AudioClip kick;
+	public AudioClip mid;
+	public AudioClip open;
+	public AudioClip perc1;
+	public AudioClip perc2;
+	public AudioClip rim;
+	public AudioClip shake;
 	public AudioClip snare;
-	private AudioSource audio;
 	// Use this for initialization
 	void Start () {
 		audio = GetComponent<AudioSource>();
@@ -19,8 +29,14 @@ public class SoundManager : MonoBehaviour {
 		if(e.note%4 == 0){
 			audio.PlayOneShot(kick, 1F);
 		}
-		if(e.note%2 == 0){
-			//audio.PlayOneShot(snare, 1F);
+		if(e.note == 4){
+			audio.PlayOneShot(snare, 1F);
+		}
+		if(e.note == 3){
+			audio.PlayOneShot(perc1, 1F);
+		}
+		if(e.note == 2 || e.note == 2){
+			audio.PlayOneShot(rim, 1F);
 		}
 	}
 }
