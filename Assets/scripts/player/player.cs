2db97a2548fe6dc534aa9fbfcbff10e0fb997e12
Frank Baele <frankbaele@gmail.com>
2016-01-30 15:16:05 +0100

    remove incontrol

--- Assets/scripts/player/player.cs
+++ Assets/scripts/player/player.cs
@@ -1,9 +1,12 @@
 ﻿using UnityEngine;
-using InControl;
 using System.Collections;
 public class Player : MonoBehaviour
 {
-	public int id;
+	public string id;
+	bool left = false;
+	bool right = false;
+	bool up = false;
+	bool down = false;
 	void Start()
 	{
 		Events.instance.AddListener<TimerTick>(tick);
@@ -11,8 +14,48 @@ public class Player : MonoBehaviour
 	
 	void Update()
 	{
+		left = false;
+		right = false;
+		up = false;
+		down = false;
 		
+		if(Input.anyKey)
+		{
+			if (Input.GetButtonDown("Up_" + id))
+				up = true;
+			if (Input.GetButtonDown("Down_" + id))
+				down = true;
+			if (Input.GetButtonDown("Left_" + id))
+				left = true;
+			if (Input.GetButtonDown("Right_" + id))
+				right = true;
+		}
+		var dpadVert = Input.GetAxis("Vertical_" + id);
+		if(dpadVert == 1){
+			up = true;
+		} else if( dpadVert == -1){
+			down = true;
+		}
+		var dpadHor = Input.GetAxis("Horizontal_" + id);
+		if(dpadHor == -1){
+			left = true;
+		} else if( dpadHor == 1){
+			right = true;
+		}
+		if(left){
+			Debug.Log("left");
+		}
+		if(right){
+			Debug.Log("right");
+		}
+		if(up){
+			Debug.Log("up");
+		}
+		if(down){
+			Debug.Log("down");
+		}
 	}
+
 	void tick(TimerTick e){
 		
 	}
